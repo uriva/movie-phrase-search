4b1b32a171649b6465240c57d08f883cd064bc84
Uri <uri.valevski@gmail.com>
2022-10-04 13:25:30 +0300

    multiple-movies

--- src/index.js
+++ src/index.js
@@ -255,23 +255,29 @@ const main = async ({
 
 (async () => {
   const webTorrentClient = new WebTorrent();
-  await main({
-    searchParams: {
-      name: "the fellowship of the ring 1080p",
-      phrase: "a hobbit",
-    },
-    webTorrentClient,
-    magnet: { maxResults: 1, medium: "Movies" },
-    srt: {
-      // path: "/home/uri/Downloads/pretty-woman-1990-english-yify-129600/Pretty.Woman.1990.1080p.720p.BluRay.x264.[YTS.MX]-English.srt",
-      // imdbid: "tt7768848",
-      limit: 1,
-    },
-    downloadParams: {
-      bufferLeft: 0,
-      bufferRight: 0,
-    },
-  });
+  await map((name) =>
+    main({
+      searchParams: {
+        name,
+        phrase: "my precious",
+      },
+      webTorrentClient,
+      magnet: { maxResults: 1, medium: "Movies" },
+      srt: {
+        // path: "/home/uri/Downloads/pretty-woman-1990-english-yify-129600/Pretty.Woman.1990.1080p.720p.BluRay.x264.[YTS.MX]-English.srt",
+        // imdbid: "tt7768848",
+        limit: 1,
+      },
+      downloadParams: {
+        bufferLeft: 0,
+        bufferRight: 0,
+      },
+    })
+  )([
+    "the fellowship of the ring 1080p",
+    "the two towers 1080p",
+    "the return of the king 1080p",
+  ]);
   console.log("finished");
   webTorrentClient.destroy();
   process.exit();
