890dbd5a9f4e0737650f66f87bc83d1c1cffe82b
uri <uri.valevski@gmail.com>
2022-12-31 02:19:16 +0200

    searchstream

--- src/twitter.js
+++ src/twitter.js
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 
-import { TwitterApi } from "twitter-api-v2";
+import { ETwitterStreamEvent, TwitterApi } from "twitter-api-v2";
+
 import { botHelper } from "./botUtils.js";
 import { log } from "gamla";
 
@@ -11,6 +12,8 @@ export const main = async ({
   accessSecret,
   appKey,
   appSecret,
+  command,
+  bearerToken,
 }) => {
   const client = new TwitterApi({
     appKey,
@@ -18,29 +21,23 @@ export const main = async ({
     accessToken,
     accessSecret,
   });
-
-  for await (const {
-    text,
-    id,
-    referenced_tweets,
-  } of await client.v2.userMentionTimeline(process.env.userId, {
-    max_results: 5,
-    expansions: ["referenced_tweets.id"],
-  })) {
-    if (!referenced_tweets || !referenced_tweets.length) {
-      botHelper(
-        console.log,
-        async (filepath) => {
-          client.v2.reply("", id, {
-            media: {
-              media_ids: [await client.v1.uploadMedia(filepath)],
-            },
-          });
-        },
-        () => client.v2.reply("sorry i could not find it :(", id),
-      )(log(text).split("@quote2video")[1].trim());
-    }
-  }
+  const bearerTokenClient = new TwitterApi(bearerToken);
+  const stream = await bearerTokenClient.v2.searchStream();
+  stream.autoReconnect = true;
+  stream.on(ETwitterStreamEvent.Data, ({ data: { id, text } }) => {
+    console.log("incoming tweet", { id, text });
+    botHelper(
+      console.log,
+      async (filepath) => {
+        client.v2.reply("", id, {
+          media: {
+            media_ids: [await client.v1.uploadMedia(filepath)],
+          },
+        });
+      },
+      () => client.v2.reply("sorry i could not find it :(", id),
+    )(log(text).split(command)[1].trim());
+  });
 };
 
 export const printAuthLink = async ({ appKey, appSecret }) => {
@@ -71,9 +68,13 @@ export const getAccessToken = async ({
   }
 };
 
-main({
-  appKey: process.env.appKey,
-  appSecret: process.env.appSecret,
-  accessToken: process.env.accessToken,
-  accessSecret: process.env.accessSecret,
-});
+main(process.env);
+
+export const writeRules = async ({ command, bearerToken }) => {
+  const bearerTokenClient = new TwitterApi(bearerToken);
+  await bearerTokenClient.v2.updateStreamRules({
+    add: [{ value: command, tag: command }],
+  });
+  const rules = await bearerTokenClient.v2.streamRules();
+  console.log(rules.data);
+};
