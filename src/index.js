26434b7ce931fb8e3d6e0ef09738a4997e25c6b5
uri <uri.valevski@gmail.com>
2023-01-08 12:30:25 +0200

    single match in index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import WebTorrent from "webtorrent";
 import { findAndDownload } from "./phraseFinder.js";
-import { map } from "gamla";
 
 (async () => {
   const webTorrentClient = new WebTorrent();
-  await map((name) =>
-    findAndDownload({
-      searchParams: {
-        max: 1,
-        name,
-        phraseStart: "diamond lane",
-        phraseEnd: null,
-        maxSpan: 120,
-      },
-      webTorrentClient,
-      srt: {
-        language: "en",
-        limit: 1,
-      },
-      downloadParams: {
-        limit: 2,
-        offset: 73,
-        bufferLeft: 0,
-        bufferRight: 0,
-      },
-    }),
-  )(["curb your enthusiasm s04e06"]);
+  await findAndDownload({
+    searchParams: {
+      max: 1,
+      name: "alien 1979",
+
+      phraseStart: "survivor",
+      phraseEnd: "signing off",
+      maxSpan: 120,
+    },
+    webTorrentClient,
+    srt: {
+      language: "en",
+      limit: 1,
+    },
+    downloadParams: {
+      limit: 1,
+      offset: 50,
+      bufferLeft: 0,
+      bufferRight: 0,
+    },
+  });
   webTorrentClient.destroy();
   process.exit();
 })().catch(console.error);
